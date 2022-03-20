45880d354062102293c016d9ed0a91f60866573a
Aleksey Karev <laurent.94@yandex.ru>
2022-03-20 22:00:14 +0300

    Added tests for Burger.class

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,90 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BurgerTest {
+
+    Burger burger;
+
+    @Before
+    public void setUp() {
+        burger = new Burger();
+    }
+
+    @Mock
+    Bun bun;
+
+    @Mock
+    Ingredient ingredient, ingredient1, ingredient2;
+
+//    @Test
+//    public void setBunsBunCalledWithBunTypeArgumentTest() {
+//        burger.setBuns(bun);
+//        System.out.println(bun);
+//        assertEquals(null, bun);
+//    }
+
+    @Test
+    public void addIngredientOneIngredientIngredientIsAddedTest() {
+        burger.addIngredient(ingredient);
+        int expectedAmountOfIngredients = 1;
+        int actualAmountOfIngredients = burger.ingredients.size();
+        assertEquals(expectedAmountOfIngredients, actualAmountOfIngredients);
+    }
+
+    @Test
+    public void removeIngredientTwoIngredientsIngredientIsRemovedTest() {
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(0);
+        int expectedAmountOfIngredients = 0;
+        int actualAmountOfIngredients = burger.ingredients.size();
+        assertEquals(expectedAmountOfIngredients, actualAmountOfIngredients);
+    }
+
+    @Test
+    public void moveIngredientTwoIngredientsIndexOfTheFirstIngredientMovedTest() {
+        burger.addIngredient(ingredient1);
+        burger.addIngredient(ingredient2);
+        burger.moveIngredient(0,1);
+        int expectedIndexOfIngredient = 1;
+        int actualIndexOfIngredient = burger.ingredients.indexOf(ingredient1);
+        assertEquals(expectedIndexOfIngredient, actualIndexOfIngredient);
+    }
+
+    @Test
+    public void getPriceOneIngredientAndTwoBunsReturnsPriceOfTheBurgerTest() {
+        Mockito.when(bun.getPrice()).thenReturn(100f);
+        Mockito.when(ingredient.getPrice()).thenReturn(100f);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        float expectedBurgerPrice = 300f;
+        float actualBurgerPrice = burger.getPrice();
+        assertEquals(expectedBurgerPrice, actualBurgerPrice, 0.0f);
+    }
+
+    @Test
+    public void getReceipt() {
+        Mockito.when(bun.getName()).thenReturn("Флюоресцентная булка R2-D3");
+        Mockito.when(bun.getPrice()).thenReturn(100f);
+        Mockito.when(ingredient.getPrice()).thenReturn(100f);
+        Mockito.when(ingredient.getName()).thenReturn("Мясо бессмертных моллюсков Protostomia");
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.FILLING);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        String expectedReceipt = "(==== Флюоресцентная булка R2-D3 ====)\r\n" +
+                "= filling Мясо бессмертных моллюсков Protostomia =\r\n" +
+                "(==== Флюоресцентная булка R2-D3 ====)\r\n" +
+                "\r\nPrice: 300,000000\r\n";
+        String actualReceipt = burger.getReceipt();
+        assertEquals(expectedReceipt, actualReceipt);
+    }
+
+}
